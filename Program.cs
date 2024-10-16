79a6d0284537473d8d67fc9b865473fa865d7361
Thomas Castiglione <castiglione@mac.com>
2024-10-16 19:25:42 +0800

    working login

--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
+using System.Security.Claims;
 using Akycha.Components;
 using Akycha.Model;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie();
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddCascadingAuthenticationState();
 
 builder.Services.AddDbContextFactory<FactoryContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString(nameof(FactoryContext))));
@@ -27,7 +35,25 @@ else
 app.UseStaticFiles();
 app.UseAntiforgery();
 
+app.UseAuthentication();
+app.MapPost("/Account/Login", async (HttpContext context, [FromForm] LoginModel model) => { 
+    if (model.Password == "naspw")
+    {
+        await context.SignInAsync(new ClaimsPrincipal(new ClaimsIdentity([new Claim(ClaimTypes.Name, "username")], "password")), new()
+        {
+            IsPersistent = true,
+        });
+    }
+    return Results.Redirect("/");
+});
+app.MapPost("/Account/Logout", async (HttpContext context) => {
+    await context.SignOutAsync();
+    return Results.Redirect("/");
+});
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
+
+record LoginModel(string Password);
