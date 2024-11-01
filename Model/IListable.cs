313fe298365554169841e1e612b12fbdeae9f786
Thomas Castiglione <castiglione@mac.com>
2024-11-01 15:47:50 +0800

    improve & automate titles

--- Model/IListable.cs
+++ Model/IListable.cs
@@ -3,6 +3,7 @@ namespace Akycha.Model;
 public interface IListable<T>
 {
     int Id { get; set; }
+    string Name { get; }
     string Category { get; }
     static abstract IOrderedEnumerable<T> Sort(IEnumerable<T> ts);
 }
