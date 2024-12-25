6cb3aa7573f77d605699062ad9dc194f6f3a5cf3
Thomas Castiglione <castiglione@mac.com>
2024-12-25 21:42:27 +0800

    xplat deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,4 @@
 dotnet publish -c Release
-Compress-Archive -Path .\bin\Release\net8.0\publish\* -DestinationPath .\bin\Release\net8.0\publish.zip -Force
+Compress-Archive -Path ./bin/Release/net8.0/publish/* -DestinationPath ./bin/Release/net8.0/publish.zip -Force
 #az login --tenant a9c9b3ac-08e2-42f3-b550-80fbbd5cd18c
-az webapp deploy --type zip --src-path .\bin\Release\net8.0\publish.zip -g personal -n akycha
+az webapp deploy --type zip --src-path ./bin/Release/net8.0/publish.zip -g personal -n akycha
